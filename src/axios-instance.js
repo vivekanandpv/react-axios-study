0e1a3f759d091a07bec1d76314571ed6c6b2eac4
Vivekanand Pujar <vivekanand.pujar@icloud.com>
2025-04-22 17:35:09 +0530

    interceptors added

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -3,3 +3,27 @@ import axios from 'axios';
 export const httpClient = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com/',
 });
+
+httpClient.interceptors.request.use(
+  (config) => {
+    console.log('request interception');
+    config.headers.set('X-Custom-Header', 'My custom value');
+
+    return config;
+  },
+  (error) => {
+    console.log('request error');
+    return Promise.reject(error);
+  }
+);
+
+httpClient.interceptors.response.use(
+  (response) => {
+    console.log('response interception');
+    return response;
+  },
+  (error) => {
+    console.log('Response error');
+    return Promise.reject(error);
+  }
+);
